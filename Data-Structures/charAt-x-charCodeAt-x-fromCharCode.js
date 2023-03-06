2bf53f4505e7e1df53f543108097579b5348ae2f
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-03-06 19:44:27 -0300

    updated files

---
+++ Data-Structures/charAt-x-charCodeAt-x-fromCharCode.js
@@ -0,0 +1,11 @@
+// O método String.fromCharCode() retorna uma string criada ao usar uma sequência específica de valores Unicode.
+console.log(String.fromCharCode(65, 66, 67, 68, 69)); // ABCDE
+
+
+
+let name = "Eduardo Mengue Scheffer"
+// O método charAt() retorna o caractere especificado a partir de uma string.
+console.log(name.charAt(1)); // d
+
+// O método charCodeAt() retorna um número inteiro entre 0 e 65535 que representa a unidade de código UTF-16 no índice fornecido.
+console.log(name.charCodeAt(0)); // 69
---
+++ Data-Structures/parseInt-x-Number.js
@@ -0,0 +1,17 @@
+// conversão de número
+console.log(parseInt('12')); // 12
+console.log(Number('12')); // 12
+
+// conversão de valor não numerico:
+console.log(parseInt('teste')); // NaN
+console.log(Number('teste')); // NaN
+
+// conversão de float:
+console.log(parseInt('12.7')); // 12
+console.log(Number('12.7')); // 12.7
+console.log(parseFloat('12.7')); // 12.7
+
+// conversão de expoentes:
+console.log(parseInt('1e2')); // 1
+console.log(Number('1e2')); // 100
+
