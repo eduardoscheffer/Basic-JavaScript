4a1d975534c90f77a5297e5d6c538ff8dff01db8
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-03-20 19:06:14 -0300

    updated passwordChecker.js

--- Basic-Algorithms-Scripting/password-checker.js
+++ Basic-Algorithms-Scripting/password-checker.js
@@ -1,16 +1,13 @@
-const passwordChecker = (password) => {
-    let lengthLimit = 8;
-    let hasCapitalLetter = /[A-Z]/.test(password);
-    let hasUpperLetter = /[a-z]/.test(password);
-    let hasLowerCharacter = false;
-    let hasDigit = false;
-    let strangeCharacters = `!@#$%^&*()-_=+[{]}\|;:',<.>/?`;
+function passwordChecker (password) {
+    let hasMinHeight = password.length >= 8 ? true: false;
+    let hasUppercase = /[A-Z]/.test(password) ? true: false;
+    let hasLowerCase = /[a-z]/.test(password) ? true: false;
+    let hasDigit = /[0-9]/.test(password) ? true: false;
+    let hasSymbol = /\W+/.test(password) ? true: false;
 
-    if (password.length < lengthLimit) {
-        console.log('Senha muito curta');
-        return false;
-    }
-
-
-
+    return (hasMinHeight && hasUppercase && hasLowerCase && hasDigit && hasSymbol) ? `Senha forte`: `Senha fraca`;
 }
+
+console.log(passwordChecker('Eduardo')); // Senha fraca
+console.log(passwordChecker('#Ragnar2038')); // Senha Forte
+console.log(passwordChecker('90901799')); // Senha fraca
