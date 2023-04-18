ea0eee78681734330a983531004c47afa2518d17
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-04-18 12:55:12 -0300

    studied symbol type

---
+++ Data-Structures/symbol-type.js
@@ -0,0 +1,20 @@
+// A JavaScript Symbol is a primitive datatype just like Number, String, or Boolean.
+
+// It represents a unique "hidden" identifier that no other code can accidentally access.
+
+// For instance, if different coders want to add a person.id property to a person object belonging to a third-party code, they could mix each others values.
+
+// Using Symbol() to create a unique identifiers, solves this problem:
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+  
+  let id = Symbol('id');
+  person[id] = 140353;
+  
+  console.log(person[id]); // 140353
+  console.log(person); // { firstName: 'John', lastName: 'Doe', age: 50, eyeColor: 'blue' }
